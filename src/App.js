c4e125f535c18a9a136cf040fbdf482c3507b942
Carolina <ydalk3@gmail.com>
2021-10-01 16:19:40 -0500

    se preparo la estructura directorios de la app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        FrontEnd (React): Proyecto Mision TIC, Ciclo 3 (App  de seguimiento de las ventas de un producto y/o servicio en una empresa).
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React v{ React .version }
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Componente Principal</h1>
+      <p className="descripcion"> componente App preparado para iniciar</p>
+    </>
   );
 }
 
