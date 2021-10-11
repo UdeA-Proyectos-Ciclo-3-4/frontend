5b1cb12612710467c2340d81ca95b519f9daf769
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-11 12:00:01 -0500

    [+] Component (Productos): Consulta al API todos los registros

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Components
 import Menu from '../Menu';
@@ -6,16 +6,49 @@ import TableProducts from './TableProducts';
 import NavbarProduct from './NavbarProduct';
 
 // Static Data
-import initialState from '../../data/products';
+import initialState, { initialStateProducts } from '../../data/products';
+
+const apiUrl = "http://localhost:5003/api/productos";
 
 // Functional Component
 const Products = () => {
 
     // State Component
-    const [ products, setProducts ] = useState( initialState );
+    const
+        [ products, setProducts ] = useState([]);
+
+
+    useEffect( () => {
+        async function getProductsfromAPI() {
+            /** Consulta la data de productos a la API */
+            const response = await fetch( apiUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                }
+            });
+
+            /** Obtiene la data */
+            let data  = await response.json();
+            console.log( data );
+
+            /** Verifica si se obtubieron los datos existosamente */
+            if ( data.success ) {
+                console.log( data );
+                setProducts( data.productos );          // Asigna la data del API al State Component
+            }
+            else {
+                console.log(`Load static data`);
+                setProducts( initialStateProducts );    // Asigna la data por defecto (estatica) al State Component
+            }
+
+        }
+
+        getProductsfromAPI();
+
+    }, [] );
 
     const addNewProduct = ( newProduct ) => {
-        //console.log( addNewProduct, newProduct );
 
         setProducts([
             ...products,
@@ -36,7 +69,6 @@ const Products = () => {
     }
 
     const deleteProduct = ( productToBeEliminated ) => {
-        //console.log( 'deleteProduct', productToBeEliminated );
 
         setProducts( products.filter( product => (
             product[ '_id' ] !== productToBeEliminated[ '_id' ]
