3df4a93f9e2427568d08d1fd3df7365fd0dda468
Milu <lifelm2004@gmail.com>
2021-10-13 20:01:47 -0500

    se crea tabla de ventas, form add/edit y ventas

--- src/components/ventas/forms/FormAddEdit.js
+++ src/components/ventas/forms/FormAddEdit.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { Modal, Button , } from 'react-bootstrap';
+import { Modal, Button , Form} from 'react-bootstrap';
 
-//import { initialStateVendedores } from '../../../data/vendedores';
+//import { initialStateVentas } from '../../../data/ventas';
 
 // Functional Component
-const FormAddEdit = ({ type, vendedor, setStateModal, updateVendedor, addNewVendedor }) => {
+const FormAddEdit = ({ type, Venta, setStateModal, updateVenta, addNewVenta }) => {
 
     // State Component
-    const [ formVendedor, setFormVendedor ] = useState( vendedor );
+    const [ formVenta, setFormVenta ] = useState( Venta );
 
-    const { nombre, especialidad, celular } = formVendedor;
+    const { N_Factura, cliente, Descripcion, Total, FechaInicial, FechaPago, Responsable, Estado } = formVenta;
 
     const handleCloseModal = () => {
         console.log( 'Click handleCloseModal' );
@@ -17,8 +17,8 @@ const FormAddEdit = ({ type, vendedor, setStateModal, updateVendedor, addNewVend
     }
 
     const handleChange = event => {
-        setFormVendedor({
-            ...formVendedor,
+        setFormVenta({
+            ...formVenta,
             [ event.target.name ]: event.target.value
         });
     }
@@ -29,11 +29,11 @@ const FormAddEdit = ({ type, vendedor, setStateModal, updateVendedor, addNewVend
 
         
         if( type === 'edit' ) {
-            updateVendedor( formVendedor );
+            updateVenta( formVenta );
             
         }
         if( type === 'add' ) {
-            addNewVendedor( formVendedor );
+            addNewVenta( formVenta );
             
         }
     }
@@ -41,42 +41,100 @@ const FormAddEdit = ({ type, vendedor, setStateModal, updateVendedor, addNewVend
     return (
         <form onSubmit={ handleSubmit }>
             <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Nombres</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Ej. Maria"
-                    name="nombre"
-                    value={ nombre }
-                    onChange={ handleChange }
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">especialidad</label>
-                <input
-                type="text"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Ej. joyero"
-                name="especialidad"
-                value={ especialidad }
-                onChange={ handleChange }
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Celular</label>
+                <label htmlFor="exampleFormControlInput1" className="form-label">N° Factura</label>
                 <input
                     type="number"
                     className="form-control"
                     id="exampleFormControlInput1"
-                    placeholder="Ej: 313009090"
-                    name="celular"
-                    value={ celular }
+                    placeholder="Ej: 001"
+                    name="N_Factura"
+                    value={ N_Factura }
                     onChange={ handleChange }
                 />
             </div>
-           
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Cliente</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Ej. Luisa Matoma"
+                    name="cliente"
+                    value={ cliente }
+                    onChange={ handleChange }
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Descripción</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Ej. Tennis"
+                    name="Descripcion"
+                    value={ Descripcion }
+                    onChange={ handleChange }
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Total</label>
+                <input
+                    type="number"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Ej: $ 100.000"
+                    name="Total"
+                    value={ Total }
+                    onChange={ handleChange }
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Fecha Inicial</label>
+                <input
+                    type="date"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Ej. 2021-09-01"
+                    name="FechaInicial"
+                    value={ FechaInicial }
+                    onChange={ handleChange }
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Fecha Pago	</label>
+                <input
+                    type="date"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Ej. 2021-09-01"
+                    name="FechaPago"
+                    value={ FechaPago }
+                    onChange={ handleChange }
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Responsable</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Ej. Luisa"
+                    name="Responsable"
+                    value={ Responsable }
+                    onChange={ handleChange }
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Estado</label>
+                <Form.Select name = "Estado"  value = {Estado } as="select" onChange={handleChange} aria-label="Default select example">
+                    <option>Seleccione...</option>
+                    <option value="Creacion" >Creacion</option>
+                    <option value="Embalaje" >Embalaje</option>
+                    <option value="Despacho">Despacho</option>
+                    <option value="Ruta">Ruta</option>
+                    <option value="Recepcion">Recepcion</option>
+                </Form.Select>
+            </div>
 
             <Modal.Footer>
                 <Button variant="secondary" onClick={ handleCloseModal }>
